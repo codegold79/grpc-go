8c725b1f122d7bee305510af249d67270d1abd8b
Frankie Gold <fgold@vmware.com>
2020-04-02 16:31:52 -0700

    Add Ginkgo version of integration tests
    Signed-off-by: Frankie Gold <fgold@vmware.com>

---
+++ examples/route_guide/bddTests/bdd_suite_test.go
@@ -0,0 +1,13 @@
+package bddTests
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestClient(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "BDD Test Suite")
+}
